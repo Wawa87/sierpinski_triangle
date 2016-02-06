39959dcf425f8f86d8f3726d2f7e3e736bce2696
Anthony Guthrie <guthrie.anthony@gmail.com>
2016-02-06 15:09:10 -0800

    Added code to center the triangle

--- sierpinksi_triangle.py
+++ sierpinksi_triangle.py
@@ -1,44 +1,51 @@
 import turtle
 import math
 
-#Instantiate turtle object
+#Instantiate Leonardo the turtle
+window = turtle.Screen()
+window.bgcolor("#e1e1ea")
 leo = turtle.Turtle()
-
 leo.shape("turtle")
-leo.color("black", "green")
-leo.hideturtle()
+leo.color("blue", "green")
 leo.speed(10)
 
-#Recursive function to draw the triangles
 def draw_sierpinksi(size, depth):
-    if depth==0:
-        leo.begin_fill()
-        for x in range (0,3):
-            leo.forward(size)
-            leo.left(120)
-        leo.end_fill()
+    #Center the triangle on screen
+    leo.penup()
+    leo.setpos(-size/2, -size/2)
+    leo.pendown()
 
-    else:
-        draw_sierpinksi(size/2, depth-1)
+    #Recursive function to draw the triangles
+    def make_triangles(size, depth):
+        if depth==0:
+            leo.begin_fill()
+            for x in range (0,3):
+                leo.forward(size)
+                leo.left(120)
+            leo.end_fill()
 
-        #Position for second triangle
-        leo.penup()
-        leo.setpos((leo.xcor() + size/2),leo.ycor())
-        leo.pendown()
+        else:
+            make_triangles(size/2, depth-1)
 
-        draw_sierpinksi(size/2, depth-1)
+            #Position for second triangle
+            leo.penup()
+            leo.setpos((leo.xcor() + size/2),leo.ycor())
+            leo.pendown()
 
-        #Position for third triangle
-        leo.penup()
-        leo.setpos((leo.xcor()-(size/2)*math.cos(math.pi/3)),(leo.ycor()+(size/2)*math.sin(math.pi/3)))
-        leo.pendown()
+            make_triangles(size/2, depth-1)
 
-        draw_sierpinksi(size/2, depth-1)
+            #Position for third triangle
+            leo.penup()
+            leo.setpos((leo.xcor()-(size/2)*math.cos(math.pi/3)),(leo.ycor()+(size/2)*math.sin(math.pi/3)))
+            leo.pendown()
 
-        #Position at origin relative to current depth
-        leo.penup()
-        leo.setpos((leo.xcor()-(size/2)*math.cos(math.pi/3)),(leo.ycor()-(size/2)*math.sin(math.pi/3)))
-        leo.pendown()
+            make_triangles(size/2, depth-1)
 
-#Call the drawing function
-draw_sierpinksi(400, 4)
+            #Position at origin relative to current depth
+            leo.penup()
+            leo.setpos((leo.xcor()-(size/2)*math.cos(math.pi/3)),(leo.ycor()-(size/2)*math.sin(math.pi/3)))
+            leo.pendown()
+
+    make_triangles(size, depth)
+
+draw_sierpinksi(400, 2)
